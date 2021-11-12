147dc742934a32e4279b6d2b605fcb849e4177a2
Developer3 <Developer3@DESKTOP-UV8K4BI>
2021-11-12 08:42:46 +0300

    ang

--- UIwebAngular/testUI/angularTest/src/app/get-patient/get-patient.component.ts
+++ UIwebAngular/testUI/angularTest/src/app/get-patient/get-patient.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Params } from '@angular/router';
+import { Data, Params } from '@angular/router';
 import { Observable } from 'rxjs';
 import { patient } from '../models/patient';
 
@@ -10,17 +10,25 @@ import { patient } from '../models/patient';
   styleUrls: ['./get-patient.component.css']
 })
 export class GetPatientComponent  {
-  public Patients : any;
-  constructor(private http : HttpClient) {  }
+   
+ // Patients:any;
+  test: Array<any>;
+  constructor(private http : HttpClient) {  this.test = new Array<any>()}
   
+
   onSubmit(data:any){
     
     console.warn(data);
     //const headers = { 'Authorization': 'Bearer my-token', 'My-Custom-Header': 'foobar' }
     const _httpHeaders = new HttpHeaders();
     _httpHeaders.append('content-type','application/json');
-    this.http.get<patient[]>('https://localhost:44334/api/GetPatient',{headers:_httpHeaders,params:data}).subscribe((data)=>{
-      this.Patients = data;
+    this.http.get<patient[]>('https://localhost:44334/api/GetPatient',{headers:_httpHeaders,params:data})
+    .subscribe((result)=>{
+      console.warn(result);
+
+      this.test = result;
+      console.warn(this.test);
+     
   });
     
   }
